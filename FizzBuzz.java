9ab3e820943f1ad756c823f770eb9094f16f1d41
meenereem <dng4@wisc.edu>
2016-08-15 21:01:03 -0400

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,12 +3,12 @@
 public class FizzBuzz {
 	public static void main(String[] args){
 		for(int i = 1; i < 101; i++){
-			if(i%3 == 0){
-				System.out.println("Fizz");
+			if(i%15 == 0){
+				System.out.println("FizzBuzz");
 			}if(i%5 == 0){
 				System.out.println("Buzz");
-			}else if(i%15 == 0){
-				System.out.println("FizzBuzz");
+			}else if(i%3 == 0){
+				System.out.println("Fizz");
 			}else 
 				System.out.println(i);
 		}
